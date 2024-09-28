6d803af23e1b358b222b45f15ef6028fd6351160
Crowdbotics <team@crowdbotics.com>
2024-09-28 00:22:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "agrifood-trade-conn-49525",
+  url: "https://agrifood-trade-conn-49525.botics.co",
+  api: "https://agrifood-trade-conn-49525.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
